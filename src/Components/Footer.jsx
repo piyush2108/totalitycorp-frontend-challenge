ee0f629b49d774d6a830fef9725ab7708d6cf3c1
github_piyush2108 <piyusharmap@gmail.com>
2023-09-09 23:40:39 +0530

    Added product listing and about section

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,83 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShekelSign } from '@fortawesome/free-solid-svg-icons'
+
+import ToTop from './ToTop'
+
+function Footer() {
+  return (
+    <div className="py-10 px-[10%] bg-gradient-to-b from-white via-secondaryWhite to-primaryRed/50 font-primary flex flex-col md:flex-row justify-between items-center cursor-default relative">
+      <div className="py-12 text-shadeLight flex flex-col justify-center items-center gap-4">
+         <FontAwesomeIcon className="text-3xl" icon={faShekelSign} />
+         <h1 className="text-5xl">SwiftBasket</h1>
+      </div>
+
+      <div className="w-full md:w-1/2 flex justify-between gap-2">
+        <ul>
+          <li className="mb-4 text-lg opacity-80 hover:opacity-100">
+            Company
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            About Us
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Swift Plus
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Career
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Team
+          </li>
+        </ul>
+
+        <ul>
+          <li className="mb-4 text-lg opacity-80 hover:opacity-100">
+            Contact
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Help & Support
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Partner with Us
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Blogs
+          </li>
+        </ul>
+
+        <ul>
+          <li className="mb-4 text-lg opacity-80 hover:opacity-100">
+            Legal
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Terms & Conditions
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Refund Policy
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Privacy Policy
+          </li>
+
+          <li className="mt-2 text-base text-black cursor-pointer hover:underline">
+            Cookie Policy
+          </li>
+        </ul>
+      </div>
+      
+      <ToTop />
+    </div>
+  )
+}
+
+export default Footer
